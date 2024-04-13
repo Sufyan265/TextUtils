7b0ed752b87b343d410403102b4370a2b08942ff
Sufyan <Sufyan265@users.noreply.github.com>
2024-04-13 16:37:12 +0500

    remove /TextUtils path

--- src/App.js
+++ src/App.js
@@ -90,7 +90,7 @@ function App() {
             </Route> */}
 
               {/* "exact" likhny sy react router exact path match krta ha (Good practice) ↓  */}
-              <Route exact path="/TextUtils" element={
+              <Route exact path="/" element={
                 <TextForm showAlert={showAlert} heading="Type text to analyze" theme={theme} />
               } />
               <Route exact path="/about" element={
